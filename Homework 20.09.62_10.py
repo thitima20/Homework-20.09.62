65f5886e4d68669a4700b55323c980dba8cedba9
thitima20 <52314903+thitima20@users.noreply.github.com>
2019-09-21 22:18:28 +0700

    Add files via upload

---
+++ Homework 20.09.62_10.py
@@ -0,0 +1,7 @@
+#input รับค่าตัวเลขเข้ามาทางแป้นพิมพ์
+#Define of variabie กำหนดให้ตัวเลขที่รับเข้ามาทางแป้นพิมพ์เป็น int หรือจำนวนเต็ม
+number = int(input("Enter number :"))
+#process คำเงื่อนไข for โดยจะแสดงแม่สูตรคูณถึง x 12 ของตัวเลขที่รับเข้ามาทางแป้นพิมพ์
+for i in range(1,13):
+    #output เมื่อทำขั้นตอน process แล้วได้คำตอบ output ให้แสดงผลออกทางหน้าจอด้วยคำสั่ง print
+    print(number,"x",i,"=",number*i)
